ede632a6dd1d4db123f19f2168e62612948d078e
shuai <575941432@qq.com>
2021-10-03 17:41:28 +0800

    新增Student类

---
+++ git_test_01/src/main/java/com/test/Student.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @program: git_test
+ * @description:
+ * @ClassName: Student
+ * @author: liushuai
+ * @create: 2021/10/3 17:39
+ **/
+public class Student {
+
+    private String name;
+
+}
