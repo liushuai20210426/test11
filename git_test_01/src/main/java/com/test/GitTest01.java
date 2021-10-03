903e72b9256e9d0e3d4c2c314fb57d8696f7eacc
shuai <575941432@qq.com>
2021-10-03 17:00:00 +0800

    提交第一个demo

---
+++ git_test_01/src/main/java/com/test/GitTest01.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @program: git_test
+ * @description: 测试git
+ * @ClassName: GitTest01
+ * @author: liushuai
+ * @create: 2021/10/3 14:27
+ **/
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("测试git提交");
+    }
+}
